bd7f15a37c0f3cd174d7d1cb487f375d05928b28
Benedict Xavier Wanyonyi <benedictx855@gmail.com>
2024-03-18 00:08:08 +0300

    smoothed out some stuff

--- gx-viewer/index.js
+++ gx-viewer/index.js
@@ -1,1 +1,12 @@
-import { Fabric } from "./dependencies/fabric.js";
+
+const canvas = fabric.Canvas("canvas")
+
+const rect = fabric.Rect({
+    width:100,
+    height:600,
+    fill:"red",
+    top:100,
+    left:100,
+})
+
+canvas.add(rect)
