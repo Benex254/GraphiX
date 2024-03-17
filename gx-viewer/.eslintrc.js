3f1327cf3ddc7f20426f5d22771193f050383466
Benedict Xavier Wanyonyi <benedictx855@gmail.com>
2024-03-17 21:14:45 +0300

    wow i'm finally through with the basic setup

---
+++ gx-viewer/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: '../.eslintrc.js',
+//   rules: {
+//     // Add any specific rules for the gx-viewer folder if needed
+//   }
+};
---
+++ gx-viewer/index.ts
@@ -0,0 +1,1 @@
+import { Fabric } from "./dependencies/fabric.js";
